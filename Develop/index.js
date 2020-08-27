6b043a1cb65eae0f7146d4f04afb59ea0fe461bb
Calum Held <calumheld@gmail.com>
2020-08-26 22:29:07 -0500

    done

--- Develop/index.js
+++ Develop/index.js
@@ -1,55 +1,95 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is your name?",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "Where are you from?",
-            name: "location"
-        },
-        {
-            type: "input",
-            message: "Tell me about yourself.",
-            name: "bio"
-        },
-        {
-            type: "input",
-            message: "What is your linkned in url?",
-            name: "linkedin"
-        },
-        {
-            type: "input",
-            message: "What is your GitHub url?",
-            name: "github"
-        }
-    ]).then(function (answers) {
-        const { name, location, bio, linkedin, github } = answers
-        const markup = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <h1> Hello my name is ${name}! </h1>
-            <h3> I am from ${location}. </h3>
-            <div>
-                <p> Bio: ${bio}. </p>
-                <p> Linkedin: ${linkedin} </p>
-                <p> GitHub: ${github} </p>
-            </div>
-        </body>
-        </html>
-        `
-        fs.writeFile("index.html", markup, function (err) {
-            if (err) throw err;
-            console.log("Success!");
-        })
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        message: "What is the project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Description:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Installation:",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Usage:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Contributing:",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Tests",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "what is your email address?",
+        name: "email"
+    }
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+inquirer.prompt(questions)
+.then(function(answers){
+    console.log("this works");
+    const{title,description,installation,usage,contributing,tests,github,email}=answers;
+    console.log(title);
+    const markup=
+    `# ${title}
+# Table of Contents
+1. [Description](#Description)
+2. [Installation](#Installation)
+3. [Usage](#Usage)
+4. [Contributing](#Contributing)
+5. [Tests](#Tests)
+6. [Questions](#Questions)
+
+## Description 
+${description}
+
+## Installation  
+${installation}
+
+## Usage 
+${usage}
+
+## Contributing
+${contributing}
+
+## Tests 
+${tests}
+
+## Questions
+    github: https://github.com/${github}
+    email: ${email}
+    `
+    fs.writeFile("README.md", markup, function (err) {
+        if (err) throw err;
+        console.log("Success!");
     })
+})
+}
+
+// function call to initialize program
+init();
